c73d0172cc29632d77e3e9c83b375f5db91c9758
Ryan Kestral <ryan.wise@kestral.com.au>
2020-10-01 17:09:18 +1000

    Ex 3&4

---
+++ javahard/EscapeSequencesComments.java
@@ -0,0 +1,18 @@
+public class EscapeSequencesComments {
+	public static void main(String[] args) {
+		//This excercise demonstrates escape sequences & comments (like these)!
+		System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n");
+		System.out.print("\tLearn Java the \"Hard\" Way!\n");
+		//System.out.print("Learn Java the Hard Way");
+		System.out.print("Hello\n"); //This line prints Hello.
+		System.out.print("Jello\by\n"); // This line prints Jelly.
+		/* The quick brown fox jumps over the lazy dog.
+		Quick wafting zephrys vex bold Jim */
+		System.out /* Testing */ .println("Hard to believe eh?");
+		System.out.println("Surprised? /* abcde */ Or what did you expect?");
+		System.out.println("\\ // -=- \\ //");
+		System.out.println("\\\\ \\\\\\ \\\\\\\\"); // it takes 2 to make 1!
+		System.out.println("I hope you understand \"escape sequences\" now. \n");
+		// and comments :)
+	}
+}	
